9107220b218fa60b290ca8566221ff4f6221488d
“Ranjit <ranjit.saimbi@gmail.com>
2019-04-03 16:11:55 +0100

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  attr_reader :name, :type, :booty
+  @@ships = []
+
+  def initialize(details)
+    @name = details[:name]
+    @type = details[:type]
+    @booty = details[:booty]
+    self.class.all << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships = []
+  end
 end
